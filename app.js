285fd15958392bd8d68835fec07c0cc1748bf9d1
Nishant <nishant3151998@gmail.com>
2021-04-20 23:55:03 +0530

    🎨 basic scalleton;

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("wow");
+// veriables
+let count=0;
+const element='#app';
+
+// Cashing and retreving element from DOM
+const rootElement=document.querySelector('#app');
+const increaseButton=document.querySelector('#increment');
+const decrementButton=document.querySelector('#decr');
+const display=document.querySelector('#display');
+
+// setup
+increaseButton.addEventListener('click',inc);
+decrementButton.addEventListener('click',dec);
+render();
+//functions
+function inc(){
+    count+=1;
+    render();
+}
+function dec(){
+    count-=1;
+    render();
+}
+function render(){
+    display.textContent=count;
+}
